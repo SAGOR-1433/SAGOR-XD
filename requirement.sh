25f8bb67d6d4559195a6dffa0367cf993b6913c7
X1_SAGOR_69 <121441643+X1-SAGOR-69@users.noreply.github.com>
2024-03-04 23:09:29 +0600

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,18 @@
+import os.sys
+os.system("clear")
+print("setting up tools")
+apt update -y
+apt upgrade -y
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install git -y
+pkg install espeak -y
+pip install bs4
+pip install requests
+pip install rich
+pip install httpx
+pip install beautifulsoup4
+pip install mechanize 
+pip install future
+pip install lolcat
